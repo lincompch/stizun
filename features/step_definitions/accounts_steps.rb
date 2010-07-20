53c31d8123425286f7cfa4d829851bc88df53bee
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-07-20 17:09:46 +0200

    Added tests for account creation.

--- features/step_definitions/accounts_steps.rb
+++ features/step_definitions/accounts_steps.rb
@@ -1,4 +1,18 @@
-Given /^The following accounts exist:$/ do |table|  
+Given /^the following accounts exist:$/ do |table|
 
+  table.hashes.each do |a|
+    
+    if a['type'] != "inherited" and a['parent'] == "none"
+      Account.create(:name => a['name'],
+                     :type_constant => a['type'].constantize
+                    )
+    else
+      parent = Account.find_by_name(a['parent'])
+      acc = Account.create(:name => a['name'])
+      acc.parent = parent
+      acc.save
+    end
+    
+  end
 
 end
