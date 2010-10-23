8479effc886f4c29e3c5bfd0b3837aeedbe59dce
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-10-23 12:38:17 +0200

    Extending core BigDecimal class to allow price-friendly rounding on all prices in the system.

---
+++ config/initializers/bigdecimal_price_rounding.rb
@@ -0,0 +1,8 @@
+
+# Allows us to round prices in a price-friendly way, e.g. to 0.05 precision
+# for Switzerland, Finland and other nations without 0.01 and 0.02 coins.
+class BigDecimal
+  def rounded(divider = 5)
+    return (self/BigDecimal("#{divider}")).round(2) * BigDecimal("#{divider}")  
+  end
+end
