4cfb73ba39d00d3e970bacb0d9ebe14a45274899
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-11-04 07:27:17 +0100

    Admin usergroup now working.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,13 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
+  
+  before_filter :admins_only
+  
+  def admins_only
+    if current_user.nil? or !current_user.is_admin?
+      # Silently redirect, no need to tell anyone why. If they're
+      # not an admin, they have no business here
+      redirect_to root_path
+    end
+  end
 end
