9f4285e6e8b4b5903fbfe40e8bc18f5074965c25
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-11-06 13:43:28 +0100

    Attempts to style and translate user session error messages.

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,5 +1,12 @@
 class UserSession < Authlogic::Session::Base
   
+  
+  validate do |user_session|
+    user_session.errors.add_to_base(I18n.t("stizun.user_sessions.email_cant_be_blank")) if user_session.email.blank?
+    user_session.errors.add_to_base(I18n.t("stizun.user_sessions.password_cant_be_blank")) if user_session.password.blank?
+
+  end
+  
   find_by_login_method :find_by_login_or_email
   
 end
