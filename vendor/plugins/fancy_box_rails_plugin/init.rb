8bf756448f077d06dd422e503daebcb10d9808eb
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-11-08 07:32:33 +0100

    Removing fancybox Rails plugin (seems outdated)

--- vendor/plugins/fancy_box_rails_plugin/init.rb
+++
@@ -1,2 +0,0 @@
-require 'fancy_box'
-ActionView::Base.send :include, FancyBox
--- vendor/plugins/fancy_box_rails_plugin/lib/constants.rb
+++
@@ -1,5 +0,0 @@
-module Constants
-  # list of top level domains, add more as you see fit...
-  TLDS = /net|com|org|edu|info|cc|eu|fr|tv|biz|ca|pro|cat|asia|aero|int|jobs/
-  IMAGE_TYPES = %w(jpg gif jpeg png)
-end
