7d747bc2d43045b709d565e4056f9bb90105bb05
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-11-26 10:40:44 +0100

    Slowly approaching a Rails 3.0 upgrade.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.beta3'
+
+gem 'authlogic', '2.1.6'
+gem 'searchlogic'
+gem 'will_paginate', '2.3.15'
+gem 'uuidtools'
+gem 'fastercsv', '1.5.3'
+gem 'later_dude'
---
+++ config/routes.rails3.rb
@@ -0,0 +1,86 @@
+Stizun::Application.routes.draw do
+  resources :addresses
+  resources :orders
+  resources :products
+  resources :invoices
+  resource :cart
+  resource :account
+  resources :user_sessions
+  match 'login' => 'user_sessions#new', :as => :login
+  match 'logout' => 'user_sessions#destroy', :as => :logout
+  resources :categories do
+  
+  
+      resources :products
+  end
+
+  resource :account
+  resources :users do
+  
+  
+      resources :carts
+    resources :orders
+    resources :invoices
+    resources :accounts
+    resources :addresses
+  end
+
+  resources :shipping_rates do
+  
+  
+      resources :shipping_costs
+  end
+
+  match '/admin' => 'admin/dashboard#index', :as => :admin
+  namespace :admin do
+      resources :products do
+    
+    
+          resources :components
+    end
+      resources :tax_classes
+      resources :orders
+      resources :countries
+      resources :invoices do
+        collection do
+    post :create_from_order
+    end
+    
+    
+    end
+      resources :shipping_costs
+      resources :shipping_rates
+      resources :histories
+      resources :payment_methods
+      resources :configuration_items
+      resources :users do
+    
+    
+          resources :accounts
+      resources :addresses
+    end
+      resources :journal_entries
+      resources :account_transactions
+      resources :accounts do
+    
+    
+          resources :account_transactions
+      resources :journal_entries
+    end
+      resources :categories do
+    
+    
+          resources :products
+    end
+      resources :suppliers do
+    
+    
+          resources :supply_items
+      resources :shipping_rates
+    end
+  end
+
+  match 'invoice/:uuid' => 'invoices#uuid'
+  match '/' => 'page#index'
+  match '/:controller(/:action(/:id))'
+end
