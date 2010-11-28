14c1c573326a9acb5aff876ce50c461fda53a8fa
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-11-28 22:12:38 +0100

    Force presence of shipping rate on suppliers.

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,8 +1,13 @@
 class Supplier < ActiveRecord::Base
+
+  # === Associations
   belongs_to :shipping_rate
   belongs_to :address
   has_many :categories
   has_many :products
   has_many :supply_items
 
+  # === Validations
+  validates_presence_of :supply_rate_id
+  
 end
