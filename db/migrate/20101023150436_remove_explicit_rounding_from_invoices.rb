99ed01cac44c5058c473e192c2abaa39e3405e34
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-12-10 07:57:41 +0100

    It appears that migration was out of its mind.

--- db/migrate/20101023150436_remove_explicit_rounding_from_invoices.rb
+++ db/migrate/20101023150436_remove_explicit_rounding_from_invoices.rb
@@ -1,7 +1,6 @@
 class RemoveExplicitRoundingFromInvoices < ActiveRecord::Migration
   def self.up
     remove_column :invoice_lines, :rounded_price
-    remove_column :invoice_lines, :rounded_single_price
     add_column :invoice_lines, :price, :decimal
     add_column :invoice_lines, :single_price, :decimal
 
@@ -9,7 +8,6 @@ class RemoveExplicitRoundingFromInvoices < ActiveRecord::Migration
 
   def self.down
     add_column :invoice_lines, :rounded_price, :decimal
-    add_column :invoice_lines, :rounded_single_price, :decimal
     remove_column :invoice_lines, :single_price
     remove_column :invoice_lines, :price
 
