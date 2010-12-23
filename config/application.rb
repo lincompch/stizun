25f940ba7be0aaf39e6b6ee3db7902dff0a76b66
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-12-23 08:18:36 +0100

    Specify locale explicitly

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,10 @@ module Stizun
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :"de-CH"
+    
+    # For some reason, on some servers Rails is stubborn and will _not_ respect default_locale. That's
+    # why we set locale explicitly.
+    config.i18n.locale = :"de-CH"
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
