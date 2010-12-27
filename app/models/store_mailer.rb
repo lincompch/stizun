7f0ea57cdbbcbd43f7fbc15f06eee0edb54f47de
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-12-27 20:21:57 +0100

    Fixing StoreMailer.

--- app/models/store_mailer.rb
+++ app/models/store_mailer.rb
@@ -8,12 +8,14 @@ class StoreMailer < ActionMailer::Base
     #recipients  order.notification_email_addresses
     #subject    
     #body       :user => user, :order => order
+    @user = user
+    @order = order
     subject = "#{APP_CONFIG['email_subject_prefix']} #{t("stizun.store_mailer.order_confirmation_subject")}"
     
     mail(:to => order.notification_email_addresses,
          :subject => subject) do |format|
-      format.text { render :text => "This is text!" }
-      format.html { render :text => "<h1>This is HTML</h1>" }
+      format.text #{ render :text => "This is text!" }
+      #format.html #{ render :text => "<h1>This is HTML</h1>" }
     end
   end
 
@@ -22,11 +24,11 @@ class StoreMailer < ActionMailer::Base
     #recipients  invoice.notification_email_addresses
     #body       :invoice => invoice
     subject = "#{APP_CONFIG['email_subject_prefix']} #{t("stizun.store_mailer.invoice_notification_subject")}"
-
+    @invoice = invoice
     mail(:to => invoice.notification_email_addresses,
          :subject => subject) do |format|
-      format.text { render :text => "This is text!" }
-      format.html { render :text => "<h1>This is HTML</h1>" }
+      format.text #{ render :text => "This is text!" }
+      #format.html #{ render :text => "<h1>This is HTML</h1>" }
     end
   end
 
