0e3dadaea80a0f29c73cc8243a814c483dd125ca
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-01-09 16:04:02 +0100

    More intelligent image display, only resize product pictures if they are large enough. Center main picture on display.

--- app/models/product_picture.rb
+++ app/models/product_picture.rb
@@ -5,7 +5,7 @@ class ProductPicture < ActiveRecord::Base
   
   belongs_to :product
   
-  has_attached_file :file, :styles => { :medium => ["400x400>", :jpg], :thumb => ["80x80>", :jpg] },
+  has_attached_file :file, :styles => { :medium => ["400>x400>", :jpg], :thumb => ["80>x80>", :jpg] },
                             :convert_options => {
                                 :all => "-strip"
                               }
