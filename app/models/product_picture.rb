2c99378cd201235c38551cba11b4b3574630a886
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-01-10 16:32:08 +0100

    Default background is now white.

--- app/models/product_picture.rb
+++ app/models/product_picture.rb
@@ -5,9 +5,9 @@ class ProductPicture < ActiveRecord::Base
   
   belongs_to :product
   
-  has_attached_file :file, :styles => { :medium => ["400>x400>", :jpg], :thumb => ["80>x80>", :jpg] },
+  has_attached_file :file,  :styles => { :medium => ["400>x400>", :jpg], :thumb => ["80>x80>", :jpg] },
                             :convert_options => {
-                                :all => "-strip"
+                                :all => "-strip -background white"
                               }
 
                               
