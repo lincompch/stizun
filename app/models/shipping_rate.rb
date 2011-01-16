d1b6cd2f8975f338c9adecac9122f59bdd964e24
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-01-16 16:30:16 +0100

    New default shipping rate.

--- app/models/shipping_rate.rb
+++ app/models/shipping_rate.rb
@@ -183,11 +183,11 @@ class ShippingRate < ActiveRecord::Base
       sr.tax_class = TaxClass.find_or_create_by_percentage("8.0")
       # Source: http://www.post.ch/post-startseite/post-privatkunden/post-versenden/post-pakete-inland/post-pakete-inland-preise.htm
       
-      sr.shipping_costs << ShippingCost.new(:weight_min => 0, :weight_max => 2000, :price => 8.0)
-      sr.shipping_costs << ShippingCost.new(:weight_min => 2001, :weight_max => 5000, :price => 10.0)
-      sr.shipping_costs << ShippingCost.new(:weight_min => 5001, :weight_max => 10000, :price => 13.0)
-      sr.shipping_costs << ShippingCost.new(:weight_min => 10001, :weight_max => 20000, :price => 19.0)
-      sr.shipping_costs << ShippingCost.new(:weight_min => 20001, :weight_max => 30000, :price => 26.0)
+      sr.shipping_costs << ShippingCost.new(:weight_min => 0, :weight_max => 2000, :price => 8.35)
+      sr.shipping_costs << ShippingCost.new(:weight_min => 2001, :weight_max => 5000, :price => 10.20)
+      sr.shipping_costs << ShippingCost.new(:weight_min => 5001, :weight_max => 10000, :price => 11.10)
+      sr.shipping_costs << ShippingCost.new(:weight_min => 10001, :weight_max => 20000, :price => 16.70)
+      sr.shipping_costs << ShippingCost.new(:weight_min => 20001, :weight_max => 30000, :price => 23.15)
       
       # This default tax class for shipping would need to be made configurable in the 
       # final system, especially if it is going to be released as Free Software.
