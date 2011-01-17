88db48e4a65fb1bf129d6d413cedb8bf5fc6fa20
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-01-17 14:29:40 +0100

    Don't show links on top-level categories. in the frontend.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,7 +57,11 @@ module ApplicationHelper
           if admin == true
             output += "\t<li class=\"#{li_classname}\">" + link_to(node.name, admin_category_products_path(node)) + "</li>\n"
           else
-            output += "\t<li class=\"#{li_classname}\">" + link_to(node.name, category_products_path(node)) + "</li>\n"
+            if node.children.count > 0
+              output += "\t<li class=\"#{li_classname}\">" + link_to(node.name, category_products_path(node)) + "</li>\n"
+            else
+              output += "\t<li class=\"#{li_classname}\">#{node.name}</li>\n"
+            end
           end
           loopcount += 1
           output += tree_recursive(node.children, node.id, admin, loopcount) unless node.children.empty?
