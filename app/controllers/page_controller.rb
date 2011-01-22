964c55e50cdb5fa2847814bc63bfeaf55e3118cd
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-01-22 11:40:50 +0100

    Custom page path method.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,12 +1,17 @@
 class PageController < ApplicationController
+
   def index
+    render_custom_page(self.action_name.to_s)
   end
 
   def tos
-    path = Rails.root + 'custom/pages/tos.html.erb'
+    render_custom_page(self.action_name.to_s)
+  end
+
+  def render_custom_page(page)
+    path = Rails.root + "custom/pages/#{page}.html.erb"
     if path.exist?
       render path.to_s
     end
   end
-  
 end
