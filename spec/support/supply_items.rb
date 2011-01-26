1f6cc974cd461d02473209f50494ee1b5e4784ad
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-01-26 18:37:04 +0100

    First specs working, but needs database cleaner.

--- spec/support/supply_items.rb
+++ spec/support/supply_items.rb
@@ -1,11 +1,23 @@
-def create_supply_items
+def create_supply_items(array = [])
   
   supplier = create_supplier("Alltron AG")
   
   tc = TaxClass.find_or_create_by_percentage(:percentage => 8.0, :name => 8.0)
   
-  SupplyItem.create(:name => 'BD-Drive', :purchase_price => 250.0, :weight => 2.0, :tax_class => tc)
-  SupplyItem.create(:name => 'CPU', :purchase_price => 50.0, :weight => 4.0, :tax_class => tc)
+  if array.empty?
+    SupplyItem.create(:name => 'BD-Drive', :purchase_price => 250.0, :weight => 2.0, :tax_class => tc)
+    SupplyItem.create(:name => 'CPU', :purchase_price => 50.0, :weight => 4.0, :tax_class => tc)
+  else
+    array.each do |si|
+      SupplyItem.create(:name => si['name'], 
+                        :purchase_price => si['purchase_price'], 
+                        :weight => si['weight'], 
+                        :tax_class => TaxClass.find_by_percentage(si['tax_percentage']))
+    end
+  
+  end
+  
+  
   return SupplyItem.all
   
 end
