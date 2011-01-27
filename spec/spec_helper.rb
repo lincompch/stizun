9f9d824f27e74f93293d374dca257b06190f2a23
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-01-27 08:24:45 +0100

    Adding DatabaseCleaner to tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,5 +23,25 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 end
+
+# Include DatabaseCleaner automatically
+
+Spec::Runner.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
+
