4366cfdd30c70f2f00ea2d2f8b2fdcc168235fcf
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-01-28 21:06:17 +0100

    Some more supply item tests.

--- spec/alltron_import_spec.rb
+++ spec/alltron_import_spec.rb
@@ -40,22 +40,29 @@ describe AlltronUtil do
   describe "importing supply items from CSV" do
     
     it "should import 500 items" do
-        AlltronTestHelper.import_from_file(Rails.root + "spec/data/500_products.csv")
-        SupplyItem.count.should == 500
-        
-                         # supplier_product_code, weight, purchase_price, stock
-        supply_item_should_be(1289, 0.54, 40.38, 4)
-        supply_item_should_be(2313, 0.06, 24.49, 3)
-        supply_item_should_be(3188, 0.28, 36.90, 55)
-        supply_item_should_be(5509, 0.08, 19.80, 545)
-        supply_item_should_be(6591, 0.07, 20.91, 2)
+      AlltronTestHelper.import_from_file(Rails.root + "spec/data/500_products.csv")
+      SupplyItem.count.should == 500
+      
+                        # supplier_product_code, weight, purchase_price, stock
+      supply_item_should_be(1289, 0.54, 40.38, 4)
+      supply_item_should_be(2313, 0.06, 24.49, 3)
+      supply_item_should_be(3188, 0.28, 36.90, 55)
+      supply_item_should_be(5509, 0.08, 19.80, 545)
+      supply_item_should_be(6591, 0.07, 20.91, 2)
 
-        History.where(:text => "Supply item added during sync: 2313 Tinte Canon BJC 2000/4x00/5000 Nachfüllpatrone farbig",
-                      :type_const => History::SUPPLY_ITEM_CHANGE).first.nil?.should == false
+      History.where(:text => "Supply item added during sync: 2313 Tinte Canon BJC 2000/4x00/5000 Nachfüllpatrone farbig",
+                    :type_const => History::SUPPLY_ITEM_CHANGE).first.nil?.should == false
     end
     
     it "should change items when they have changed in the CSV file" do
-      
+      AlltronTestHelper.import_from_file(Rails.root + "spec/data/500_products_with_5_changes.csv")
+      SupplyItem.count.should == 500
+      supply_item_should_be(2313, 0.06, 24.49, 100)
+      supply_item_should_be(1289, 0.50, 40.00, 4)
+      supply_item_should_be(3188, 0.50, 36.90, 55)
+      supply_item_should_be(5509, 0.50, 25.00, 545)
+      supply_item_should_be(6591, 2.00, 49.00, 18)
+   
     end
     
     it "should mark items as deleted when they were removed from the CSV file" do
@@ -68,18 +75,7 @@ describe AlltronUtil do
     
     
   end
-  
-#    
-#   Scenario: Importing list with 5 changes
-#     When I import the file "features/data/500_products_with_5_changes.csv"
-#     Then there are 500 supply items in the database
-#     And the following supply items should exist:
-#     |product_code|weight|price|stock|
-#     |        1289|  0.54|40.00|    4|
-#     |        2313|  0.06|24.49|  100|
-#     |        3188|  0.50|36.90|   55|    
-#     |        5509|  0.50|25.00|  545|    
-#     |        6591|  2.00|40.00|   18|        
+      
 #   
 #   Scenario: Importing list with 15 missing products
 #     When I import the file "features/data/500_products.csv"
