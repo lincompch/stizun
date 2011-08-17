e5fdac0f1334b3b93ebba3c8830f630090ae6a4d
Ramón Cahenzli <rca@psy-q.ch>
2011-08-17 15:04:09 +0200

    Differentiate between MySQL and everything else.

--- db/migrate/20110228102936_allow_longer_history_text.rb
+++ db/migrate/20110228102936_allow_longer_history_text.rb
@@ -1,11 +1,17 @@
 class AllowLongerHistoryText < ActiveRecord::Migration
   def self.up
-
-    execute "ALTER TABLE histories CHANGE text text text"
-
+    if ActiveRecord::Base.connection.adapter_name == 'MySQL'
+      execute "ALTER TABLE histories CHANGE text text text"
+    else
+      execute "ALTER TABLE histories ALTER text SET DATA TYPE text"
+    end
   end
 
   def self.down
-    execute "ALTER TABLE histories CHANGE text text string"
+    if ActiveRecord::Base.connection.adapter_name == 'MySQL'
+      execute "ALTER TABLE histories CHANGE text text string"
+    else
+      execute "ALTER TABLE histories ALTER text SET DATA TYPE string"
+    end
   end
 end
