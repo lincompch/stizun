c17780a4397068cd169f99fadfa3e90d96f4e3ef
Ramón Cahenzli <rca@psy-q.ch>
2011-08-17 14:52:22 +0200

    For PostgreSQL compatibility.

--- db/migrate/20100220163423_add_stock_and_avaiable_to_products.rb
+++ db/migrate/20100220163423_add_stock_and_avaiable_to_products.rb
@@ -1,7 +1,7 @@
 class AddStockAndAvaiableToProducts < ActiveRecord::Migration
   def self.up
     add_column :products, :stock, :integer, :default => 0
-    add_column :products, :is_available, :boolean, :default => 1
+    add_column :products, :is_available, :boolean, :default => true
   end
 
   def self.down
