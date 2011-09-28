c257f6cc5808a166e094bd828ef783280f913cea
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-09-28 20:23:35 +0200

    Revert "Enable compiling assets so that lazy compilation works."
    This reverts commit c9f05af9d44815aeb02f83f116ff31a6f383857c.

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,6 @@ module Stizun
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
