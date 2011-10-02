4f2d4e66faf70ace5145ef3fa64445318eab5c12
Ramón Cahenzli <rca@psy-q.ch>
2011-10-02 14:53:04 +0200

    Expire assets.

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Stizun
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
 
