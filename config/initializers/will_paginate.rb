63f1bed6367406c8b2f46cc8578d7eeebbf730c6
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-10-22 17:46:24 +0200

    Trying to override the page gap method.

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,3 +1,12 @@
 WillPaginate::ViewHelpers.pagination_options[:previous_label] = '&lt;&lt;'
 WillPaginate::ViewHelpers.pagination_options[:next_label] = '&gt;&gt;'
 
+module WillPaginate
+  module ViewHelpers
+    protected
+    def gap
+      text = "&hellip;"
+      %(<span class="gap">#{text}</span>)
+    end
+  end
+end
