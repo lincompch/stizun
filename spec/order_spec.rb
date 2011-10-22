fc8769c7013a85d155d7d071d58c65760d7610ff
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-10-22 14:45:45 +0200

    Order spec skeleton.

---
+++ spec/order_spec.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Order do
+  describe "a normal order" do
+    it "should take order lines" do
+    end
+    
+    it "should be created from a cart" do
+    end
+    
+    it "should cancel its invoice when cancelled itself" do
+    end
+  end
+end
