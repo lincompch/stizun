22910cabb08d72b8de423dea14957d3cab6163cd
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-10-27 08:57:40 +0200

    RVM causes too many headaches in production mode by breaking cronjobs.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,4 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_type, :user
-set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
 require "bundler/capistrano"
-
-set :rvm_type, :system
 
 set :application, "lincomp"
 
