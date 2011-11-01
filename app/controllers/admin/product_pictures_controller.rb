59563c1ceb8784f8fb3a9d275a5548d9c011c24a
Sebastian Wojtczak <wojtczaksebastian@gmail.com>
2011-11-01 20:14:46 +0100

    Fixing file uploads [#20349909]

--- app/controllers/admin/product_pictures_controller.rb
+++ app/controllers/admin/product_pictures_controller.rb
@@ -1,65 +1,64 @@
 class Admin::ProductPicturesController < Admin::BaseController
-  
+
   def index
     @product_pictures = Product.find(params[:product_id]).product_pictures
   end
-  
+
   def show
     @product_picture = ProductPicture.find(params[:id])
   end
-  
+
   def new
     @product = Product.find(params[:product_id])
-    @product_pictures = @product.product_pictures
-    @product_picture = @product.product_pictures.new
+    @product_picture = @product.product_pictures.build
     render :layout => 'admin_blank'
   end
-  
-  
+
+
   def create
     @product = Product.find(params[:product_id])
     unless params[:product_picture][:file].blank?
-      pp = @product.product_pictures.new( params[:product_picture] )
-      if pp.save
+      @product_picture = @product.product_pictures.build(params[:product_picture])
+      if @product_picture.save!
         flash[:info] = "Product picture created"
         redirect_to edit_admin_product_path(@product)
       else
         flash[:error] = "Error saving product picture"
-        render :action => 'new'
+        render :action => 'new', :layout => 'admin_blank'
       end
     end
-    
+
   end
- 
+
   def edit
-    
+
   end
-  
-  def update  
+
+  def update
     puts "da shit dates up"
     @product_picture = ProductPicture.find(params[:id])
     @product_picture.update_attributes(params[:product_picture])
-    
+
     if params[:set_main_picture] == "true"
       @product_picture.set_main_picture
     end
-    
+
     redirect_to :back
-    
   end
-                                    
+
   def destroy
     pp = ProductPicture.find(params[:id])
     product = pp.product
-    
+
     # If there are at least two pictures, shift the main picture
     if pp.is_main_picture? and product.product_pictures.count > 1
       product.product_pictures.first.set_main_picture
     end
     pp.remove_file!
     pp.destroy
-    
+
     redirect_to edit_admin_product_path(product)
   end
-  
+
 end
+
