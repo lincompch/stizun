f9f59ca898af86538f7a571f9361aa0db30ba182
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2011-12-30 16:58:15 +0100

    New feature: Shipping cost summary.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -12,6 +12,27 @@ class PageController < ApplicationController
   def contact
     render_custom_page(self.action_name.to_s)
   end
+  
+  def shipping
+    begin
+      @currency ||= ConfigurationItem.get("currency").value
+    rescue ArgumentError
+      @currency ||= ""
+    end
+    
+    begin
+      @minimum_item_count = ConfigurationItem.get("free_shipping_minimum_items").value.to_i
+    rescue ArgumentError
+    end
+    
+    begin
+      @minimum_order_amount = BigDecimal.new(ConfigurationItem.get("free_shipping_minimum_amount").value)
+    rescue ArgumentError
+    end
+    
+    @shipping_rate = ShippingRate.get_default
+    
+  end
 
   def render_custom_page(page)
     path = Rails.root + "custom/pages/#{page}.html.erb"
