baeafc614d3d065e09c6b1a273fe31ce700fa414
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2012-01-07 12:20:41 +0100

    Big problems with the factories and the specs not running recursively.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,30 +9,33 @@ describe Order do
     mr0.margin_percentage = 5.0
     mr0.save
   
-    @tax_class = Factory.create(:tax_class, {:percentage => 8.0})
+    tax_class2 ||= Factory.create(:tax_class, {:percentage => 50.0})
+    tax_class2.percentage = 8.0
+    tax_class2.save
     
-    @shipping_rate = Factory.create(:shipping_rate, {:tax_class => @tax_class})
-    @shipping_rate.shipping_costs.create(:price => 10.0, :weight_min => 0, :weight_max => 10000, :tax_class => @tax_class)
+    shipping_rate = Factory.create(:shipping_rate, {:tax_class => tax_class2})
+    shipping_rate.shipping_costs.create(:price => 10.0, :weight_min => 0, :weight_max => 10000, :tax_class => tax_class2)
 
-    @supplier = Factory.create(:supplier, :shipping_rate => @shipping_rate)
-    @supplier.shipping_rate = @shipping_rate
-    @supplier.save
+    supplier = Factory.create(:supplier, :shipping_rate => shipping_rate)
+    supplier.shipping_rate = shipping_rate
+    supplier.save
+    
     
     Country.create(:name => "Somewhereland")
-    @address = Address.new(:street => 'Foo',
+    address = Address.new(:street => 'Foo',
                   :firstname => 'Foo',
                   :lastname => 'Bar',
                   :postalcode => '1234',
                   :city => 'Somewhere',
                   :email => 'lala@lala.com',
                   :country => Country.first)    
-    @address.save.should == true
+    address.save.should == true
     
     
     # For some reason, using FactoryGirl to create this breaks everything, it creates all associated
     # things along with it, ignores the @tax_class that we explicitly set and creates many unnecessary
     # suppliers and tax rates, which messes everything up completely.
-    p = Product.new(:name => "foo", :description => "bar", :weight => 5.5, :supplier => @supplier, :tax_class => @tax_class, :purchase_price => 120.0, :direct_shipping => true, :is_available => true)
+    p = Product.new(:name => "foo", :description => "bar", :weight => 5.5, :supplier => supplier, :tax_class => tax_class2, :purchase_price => 120.0, :direct_shipping => true, :is_available => true)
     p.save.should == true
     
   end
@@ -86,12 +89,12 @@ describe Order do
       c = Cart.new
       c.add_product(product)
       c.save
-
+            
       # Right now it still has the default shipping cost from the default shipping rate
-      c.shipping_cost.should == BigDecimal.new("10.80")
+      c.shipping_cost.should == BigDecimal.new("15.0")
       
       c.add_product(product)
-      c.shipping_cost.should == BigDecimal.new("21.60")
+      c.shipping_cost.should == BigDecimal.new("30.0")
       
       c.add_product(product)
       c.shipping_cost.should == BigDecimal.new("0.0")
@@ -107,10 +110,7 @@ describe Order do
       c.save
 
       # Right now it still has the default shipping cost from the default shipping rate
-      c.shipping_cost.should == BigDecimal.new("10.80")
-      
-      c.add_product(product)
-      c.shipping_cost.should == BigDecimal.new("21.60")
+      c.shipping_cost.should == BigDecimal.new("15.0")
       
       # Now the value goes over 300, thus triggering free shipping
       c.add_product(product)
