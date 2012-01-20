0a6ccdc7dad42a48ce20b9bc79151168e1794d3f
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2012-01-20 08:20:09 +0100

    Sorting by weight in the form.

--- app/controllers/admin/shipping_calculators_controller.rb
+++ app/controllers/admin/shipping_calculators_controller.rb
@@ -76,10 +76,11 @@ class Admin::ShippingCalculatorsController < Admin::BaseController
     @shipping_calculator.configuration.shipping_costs = []
     (0..config[:weight_min].size).each do |n|
       unless (config[:weight_min][n].blank? or config[:weight_max][n].blank? or config[:price][n].blank?)
-      @shipping_calculator.configuration.shipping_costs << 
+        @shipping_calculator.configuration.shipping_costs << 
         { :weight_min => config[:weight_min][n].to_f,
           :weight_max => config[:weight_max][n].to_f,
           :price => config[:price][n].to_f }
+        @shipping_calculator.configuration.shipping_costs.sort!{|a,b| a[:weight_min] <=> b[:weight_min]}
       end
     end
   end
