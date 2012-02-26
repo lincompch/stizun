a4ce7cd42c4d22ed78a43804802ab31d2f5c8b96
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2012-02-26 19:12:28 +0100

    Update config/initializers/bigdecimal_price_rounding.rb

--- config/initializers/bigdecimal_price_rounding.rb
+++ config/initializers/bigdecimal_price_rounding.rb
@@ -1,8 +1,7 @@
-
 # Allows us to round prices in a price-friendly way, e.g. to 0.05 precision
 # for Switzerland, Finland and other nations without 0.01 and 0.02 coins.
 class BigDecimal
   def rounded(divider = 5)
-    return (self/BigDecimal.new("#{divider}")).round(2, BigDecimal::ROUND_UP) * BigDecimal.new("#{divider}")  
+    return (self/BigDecimal.new("#{divider}")).round(2, BigDecimal::ROUND_HALF_EVEN) * BigDecimal.new("#{divider}")  
   end
 end
