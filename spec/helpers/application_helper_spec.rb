ee7db3a2624662640b667401fad60306e9ae5412
Ramón Cahenzli <rca@psy-q.ch>
2012-03-01 16:17:23 +0100

    Trying to validate our rounding.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -37,10 +37,18 @@ describe ApplicationHelper do
       pretty_price(price, "CHF", true).should == "CHF 150.22"
 
       price = BigDecimal.new("150.22")
-      pretty_price(price, "CHF").should == "CHF 150.25"
+      pretty_price(price, "CHF").should == "CHF 150.20"
 
       price = BigDecimal.new("150.22")
       pretty_price(price, "CHF", false).should == "CHF 150.22"
+
+      # Let's try some tricky things
+      price = BigDecimal.new("150.00392")
+      pretty_price(price, "CHF").should == "CHF 150.00" 
+
+      price = BigDecimal.new("150.03392")
+      pretty_price(price, "CHF").should == "CHF 150.05" 
+     
 
     end
   end
