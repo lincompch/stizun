6fddfcf7121ce93a3d4b29b0e921ab8f4eefef23
Ramón Cahenzli <rca@psy-q.ch>
2012-05-08 08:32:59 +0200

    Show orders that are processing in the admin dashboard.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,7 @@ class Admin::DashboardController <  Admin::BaseController
     @orders = Order.unprocessed.all
     @orders_to_ship = Order.to_ship.all
     @orders_to_be_paid = Order.awaiting_payment.all
+    @orders_processing = Order.processing.all
     @invoices = Invoice.unpaid.all
   end 
 
