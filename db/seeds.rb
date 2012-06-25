18c91e88d2a9c0954dbe7931993622f70a244a65
Ramón Cahenzli <rca@psy-q.ch>
2012-06-25 16:21:34 +0200

    Stop seeding this as it's now longer necessary

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,19 +33,19 @@ PaymentMethod.create(:name => 'Rechnung/Bankzahlung')
 Country.create(:name => 'Schweiz/Suisse/Svizzera/Svizra')
 
 
-sr = ShippingRate.new(:name => 'Swiss Post', :default => true)  
-sr.tax_class = TaxClass.find_or_create_by_percentage("8.0")
-# Source: http://www.post.ch/post-startseite/post-privatkunden/post-versenden/post-pakete-inland/post-pakete-inland-preise.htm
+# sr = ShippingRate.new(:name => 'Swiss Post', :default => true)  
+# sr.tax_class = TaxClass.find_or_create_by_percentage("8.0")
+# # Source: http://www.post.ch/post-startseite/post-privatkunden/post-versenden/post-pakete-inland/post-pakete-inland-preise.htm
 
-sr.shipping_costs << ShippingCost.new(:weight_min => 0, :weight_max => 2000, :price => 8.0)
-sr.shipping_costs << ShippingCost.new(:weight_min => 2001, :weight_max => 5000, :price => 10.0)
-sr.shipping_costs << ShippingCost.new(:weight_min => 5001, :weight_max => 10000, :price => 13.0)
-sr.shipping_costs << ShippingCost.new(:weight_min => 10001, :weight_max => 20000, :price => 19.0)
-sr.shipping_costs << ShippingCost.new(:weight_min => 20001, :weight_max => 30000, :price => 26.0)
+# sr.shipping_costs << ShippingCost.new(:weight_min => 0, :weight_max => 2000, :price => 8.0)
+# sr.shipping_costs << ShippingCost.new(:weight_min => 2001, :weight_max => 5000, :price => 10.0)
+# sr.shipping_costs << ShippingCost.new(:weight_min => 5001, :weight_max => 10000, :price => 13.0)
+# sr.shipping_costs << ShippingCost.new(:weight_min => 10001, :weight_max => 20000, :price => 19.0)
+# sr.shipping_costs << ShippingCost.new(:weight_min => 20001, :weight_max => 30000, :price => 26.0)
 
-# This default tax class for shipping would need to be made configurable in the 
-# final system, especially if it is going to be released as Free Software.
-sr.shipping_costs.each do |sc|
-  sc.tax_class = TaxClass.find_or_create_by_percentage("8.0")
-end
-sr.save
+# # This default tax class for shipping would need to be made configurable in the 
+# # final system, especially if it is going to be released as Free Software.
+# sr.shipping_costs.each do |sc|
+#   sc.tax_class = TaxClass.find_or_create_by_percentage("8.0")
+# end
+# sr.save
