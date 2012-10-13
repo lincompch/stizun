c667bc9e71733c5d2c0b9d462ae252adba681803
Ramón Cahenzli <rca@psy-q.ch>
2012-10-13 16:30:00 +0200

    Allow deleting of dispatchers, redirect more meaningfully.

--- app/controllers/admin/category_dispatchers_controller.rb
+++ app/controllers/admin/category_dispatchers_controller.rb
@@ -31,15 +31,28 @@ class Admin::CategoryDispatchersController < Admin::BaseController
   end
 
   def update
+    filter = params[:filter] || nil
     @category_dispatcher = CategoryDispatcher.find(params[:id])
     if @category_dispatcher.update_attributes(params[:category_dispatcher])
-      redirect_to :action => :index
+      redirect_to :action => :index, :filter => filter
       flash[:notice] = "Dispatcher '#{@category_dispatcher.to_s}' updated"
     else
-      redirect_to :action => :index
+      redirect_to :action => :index, :filter => filter
       flash[:error] = "An error occurred while setting the target category for dispatcher '#{@category_dispatcher}'"
     end
    
   end
+
+  def destroy
+    @category_dispatcher = CategoryDispatcher.find(params[:id])
+    if @category_dispatcher.destroy
+      flash[:notice] = "Dispatcher destroyed."
+    else
+      flash[:error] = "Dispatcher could not be destroyed."
+    end
+    filter = params[:filter] if params[:filter]
+    redirect_to :action => :index, :filter => filter
+  end
+
 
 end
