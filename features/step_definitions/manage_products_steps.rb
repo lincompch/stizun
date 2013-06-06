5be22006171ccceeb4985afa6b68b5c404721906
Ramón Cahenzli <rca@psy-q.ch>
2013-06-06 15:33:29 +0200

    Fixing tests.

--- features/step_definitions/manage_products_steps.rb
+++ features/step_definitions/manage_products_steps.rb
@@ -7,7 +7,7 @@ end
 
 
 When /^I wait for a fancybox to appear$/ do
-  page.has_css?("#fancybox-frame", :visible => true)
+  page.has_css?(".fancybox-iframe", :visible => true)
 end
 
 
@@ -15,78 +15,78 @@ When /^I create a product called "([^\"]*)"$/ do |arg1|
  visit admin_products_path
  create_link = all("a", :text => 'Create new product').first
  create_link.click
- within_frame "fancybox-frame" do
+ within_frame 0 do
    fill_in "Name", :with => arg1
  end 
 end
 
 When /^I fill in "([^"]*)" in the CKEditor instance "([^"]*)"$/ do |value, input_id|
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     browser = page.driver.browser
     browser.execute_script("CKEDITOR.instances['#{input_id}'].setData('#{value}');")
   end
 end
 
 When /^fill in the product description "([^\"]*)"$/ do |arg1|
- within_frame "fancybox-frame" do
+  within_frame 0 do
 #   click_link "Toggle WYSIWYG editor"
    fill_in "product_description", :with => arg1
  end
 end
 
 When /^I fill in the purchase price (\d+\.\d+)$/ do |arg1|
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     fill_in "Purchase price", :with => arg1
   end
 end
 
 When /^I fill in the margin percentage (\d+\.\d+)$/ do |arg1|
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     fill_in "Profit margin (in percent)", :with => arg1
   end
 end
 
 When /^I fill in the weight (\d+\.\d+)$/ do |arg1|
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     fill_in "Weight", :with => arg1
   end
 end
 
 When /^I select the tax class "([^\"]*)"$/ do |arg1|
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     select arg1, :from => "Tax Class"
   end
 end
 
 When /^I select the supplier "([^\"]*)"$/ do |arg1|
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     select arg1, :from => "Supplier"
   end
 end
 
 When /^I click the create button$/ do
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     candidate = all(".buttonbar input").first
     candidate.click if candidate.value == "Create new product"
   end
 end
 
 When /^I assign the product to the category "([^\"]*)"$/ do |arg1|
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     select arg1, :from => "Categories"
   end
 end
 
 
 Then /^(?:|I )should see "([^"]*)" in the fancybox$/ do |text|
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     page.should have_content(text)
   end
 end
 
 
 Then /^I should see an error message inside the fancybox$/ do
-  within_frame "fancybox-frame" do
+  within_frame 0 do
     page.has_css?("#errorExplanation", :visible => true).should == true
   end
 end
