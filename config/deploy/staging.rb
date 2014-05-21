9d9c055e567e563bf069332e3b00ca16b548294f
Ramón Cahenzli <rca@psy-q.ch>
2014-05-21 16:13:16 +0200

    Proper rake task.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -74,7 +74,7 @@ end
 
 
 task :configure_sphinx do
-  run "cd #{release_path} && RAILS_ENV=production bundle exec rake ts:conf && RAILS_ENV=production bundle exec rake ts:rebuild"
+  run "cd #{release_path} && RAILS_ENV=production bundle exec rake ts:configure && RAILS_ENV=production bundle exec rake ts:rebuild"
 end
 
 task :start_sphinx do
