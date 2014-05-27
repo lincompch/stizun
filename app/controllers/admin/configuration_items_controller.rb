e48cb03a5f82b765cf551cbd110c44b19fbb1a93
Ramón Cahenzli <rca@psy-q.ch>
2014-05-27 18:41:16 +0200

    Permit updating items.

--- app/controllers/admin/configuration_items_controller.rb
+++ app/controllers/admin/configuration_items_controller.rb
@@ -13,7 +13,7 @@ class Admin::ConfigurationItemsController < Admin::BaseController
   end
   
   def create
-    if @ci = ConfigurationItem.create(params[:configuration_item])
+    if @ci = ConfigurationItem.create(configuration_item_attributes)
       flash[:notice] = "Configuration item saved"
       redirect_to admin_configuration_items_path
     else
@@ -24,13 +24,20 @@ class Admin::ConfigurationItemsController < Admin::BaseController
   
   def update
     @ci = ConfigurationItem.find(params[:id])
-    @ci.update_attributes(params[:configuration_item])
+    @ci.update_attributes(configuration_item_attributes)
     if @ci.save
       flash[:notice] = "Item updated"
     else
       flash[:error] = "Error updating item"
     end
       redirect_to edit_admin_configuration_item_path(@ci)
+  end
+
+  private
+
+  def configuration_item_attributes
+    params.require(:configuration_item).permit(:key, :value, :name, :description)
+
   end
   
 end
