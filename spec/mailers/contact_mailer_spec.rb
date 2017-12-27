8778bbae6a2ba398989ae5efdcd92f5aeec006d1
Ramón Cahenzli <rca@psy-q.ch>
2017-12-27 15:09:00 +0100

    Use new instead of deprecated method

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -18,7 +18,7 @@ describe ContactMailer, :type => :feature do
   it "should set the right subject" do
     data = {:reason => 'question', :subject => 'Foo', :body => 'Bar'}
 
-    email = ContactMailer.send_email(@from, data).deliver
+    email = ContactMailer.send_email(@from, data).deliver_now
     email.from.should == [@from]
     email.to.should == [@default_to]
     email.subject.should == "[Frage] Foo"
@@ -27,21 +27,21 @@ describe ContactMailer, :type => :feature do
 
   it "should set 'Kein Betreff' if no subject was given" do
     data = {:subject => nil, :body => 'Foo'}
-    email = ContactMailer.send_email(@from, data).deliver
+    email = ContactMailer.send_email(@from, data).deliver_now
     email.subject.should == "[Kontakt] Kein Betreff"
     ActionMailer::Base.deliveries.size.should == 1
   end
 
   it "should set a default prefix of '[Kontakt]' if none was selected" do
     data = {:reason => 'unknown', :subject => 'Something', :body => 'Foo'}
-    email = ContactMailer.send_email(@from, data).deliver
+    email = ContactMailer.send_email(@from, data).deliver_now
     email.subject.should == "[Kontakt] Something"
     ActionMailer::Base.deliveries.size.should == 1
   end
 
   it "should include the right text in the message" do
     data = {:body => 'Foo'}
-    email = ContactMailer.send_email(@from, data).deliver
+    email = ContactMailer.send_email(@from, data).deliver_now
     email.body.to_s.should == "Liebe Lincomp\n\nIch habe folgendes Anliegen:\n\nFoo\n\nDanke für die Beantwortung und freundliche Grüsse\n"
     ActionMailer::Base.deliveries.size.should == 1
   end
