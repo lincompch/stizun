5bafb9b42bf5f1a82f19e775d73799fc1e611c3c
Ramón Cahenzli <rca@psy-q.ch>
2017-12-28 10:24:30 +0100

    Fix Ingram spec deprecations

--- spec/ingram_import_spec.rb
+++ spec/ingram_import_spec.rb
@@ -5,15 +5,14 @@ require_relative '../lib/ingram_util'
 
 describe IngramUtil do
   before(:each) do
-    SupplyItem.count.should == 0
+    expect(SupplyItem.count).to eq 0
     supplier = Supplier.find_or_create_by(:name => "Ingram Micro GmbH")
-    supplier.name.should == "Ingram Micro GmbH"
   end
 
   describe "importing supply items from CSV" do
     it "should import 370 items" do
       IngramTestHelper.import_from_file(Rails.root + "spec/data/370_im_products.csv")
-      SupplyItem.count.should == 370
+      expect(SupplyItem.count).to eq 370
       supplier = Supplier.where(:name => 'Ingram Micro GmbH').first
 
       supply_item_should_be(supplier, "0180631", { :manufacturer => 'Dymo',
@@ -39,18 +38,18 @@ describe IngramUtil do
     end
 
     it "should change items when they have changed in the CSV file" do
-      SupplyItem.count.should == 0
+      expect(SupplyItem.count).to eq 0
       IngramTestHelper.import_from_file(Rails.root + "spec/data/370_im_products.csv")
-      SupplyItem.count.should == 370
+      expect(SupplyItem.count).to eq 370
 
       codes = ["0180631","0180538","018Z055","0711186"]
       codes.each do |code|
           supply_item = SupplyItem.where(:supplier_product_code => code).first
           product = Product.new_from_supply_item(supply_item)
-          product.save.should == true
+          expect(product.save).to be true
       end
       IngramTestHelper.update_from_file(Rails.root + "spec/data/370_im_products_with_4_changes.csv")
-      SupplyItem.count.should == 370
+      expect(SupplyItem.count).to eq 370
 
       supplier = Supplier.where(:name => 'Ingram Micro GmbH').first
 
@@ -77,9 +76,9 @@ describe IngramUtil do
     end
 
     it "should mark items as deleted when they were removed from the CSV file" do
-      SupplyItem.count.should == 0
+      expect(SupplyItem.count).to eq 0
       IngramTestHelper.import_from_file(Rails.root + "spec/data/370_im_products.csv")
-      SupplyItem.count.should == 370
+      expect(SupplyItem.count).to eq 370
 
       supplier = Supplier.where(:name => 'Ingram Micro GmbH').first
       product_codes = ["0711642", "0712027", "0712259", "0712530", "0712577", "07701A5", "07701F4", "07702U8", "07702V2", "0770987"]
@@ -92,9 +91,9 @@ describe IngramUtil do
       # end
 
       IngramTestHelper.update_from_file(Rails.root + "spec/data/360_im_products.csv")
-      SupplyItem.count.should == 370 # but 10 of them marked deleted
+      expect(SupplyItem.count).to eq 370 # but 10 of them marked deleted
       # The others should *not* be deleted
-      SupplyItem.available.where(:supplier_id => supplier).count.should == 360
+      expect(SupplyItem.available.where(:supplier_id => supplier).count).to eq 360
 
       ids = SupplyItem.where(:supplier_product_code => product_codes, :supplier_id => supplier).collect(&:id)
       supply_items_should_be_marked_deleted(ids, supplier)
@@ -105,14 +104,14 @@ describe IngramUtil do
       IngramTestHelper.import_from_file(Rails.root + "spec/data/370_im_products.csv")
       supply_item = SupplyItem.where(:supplier_product_code => "0712259").first
       product = Product.new_from_supply_item(supply_item)
-      product.save.should == true
-      product.available?.should == true
+      expect(product.save).to be true
+      expect(product.available?).to be true
       IngramTestHelper.update_from_file(Rails.root + "spec/data/360_im_products.csv")
       supply_item.reload
-      supply_item.status_constant.should == SupplyItem::DELETED
+      expect(supply_item.status_constant).to eq SupplyItem::DELETED
 
       product.reload
-      product.available?.should == false
+      expect(product.available?).to be false
 
     end
 
@@ -129,21 +128,21 @@ describe IngramUtil do
 
       IngramTestHelper.update_from_file(Rails.root + "spec/data/supply_item_reappears_02.csv")
 
-      sup1.status_constant.should == SupplyItem::AVAILABLE
+      expect(sup1.status_constant).to eq SupplyItem::AVAILABLE
       sup2.reload
-      sup2.status_constant.should == SupplyItem::DELETED
+      expect(sup2.status_constant).to eq SupplyItem::DELETED
       
-      prod1.reload.is_available.should == true
-      prod2.reload.is_available.should == false
+      expect(prod1.reload.is_available).to be true
+      expect(prod2.reload.is_available).to be false
 
       IngramTestHelper.update_from_file(Rails.root + "spec/data/supply_item_reappears_03.csv")
 
-      sup1.status_constant.should == SupplyItem::AVAILABLE
+      expect(sup1.status_constant).to eq SupplyItem::AVAILABLE
       sup2.reload
-      sup2.status_constant.should == SupplyItem::AVAILABLE
+      expect(sup2.status_constant).to eq SupplyItem::AVAILABLE
       
-      prod1.reload.is_available.should == true
-      prod2.reload.is_available.should == true
+      expect(prod1.reload.is_available).to eq true
+      expect(prod2.reload.is_available).to eq true
     end
 
     # *Supply Item erhält danach wieder Stückzahl über 0*
@@ -155,20 +154,20 @@ describe IngramUtil do
 
       IngramTestHelper.update_from_file(Rails.root + "spec/data/supply_item_stock_comes_back_above_zero_02.csv")
       sup.reload
-      sup.stock.should == 0
+      expect(sup.stock).to eq 0
 
       prod.reload
-      prod.stock.should == 0
-      prod.is_available.should == true # It's available, but with 0 in stock
+      expect(prod.stock).to eq 0
+      expect(prod.is_available).to be true # It's available, but with 0 in stock
 
       IngramTestHelper.update_from_file(Rails.root + "spec/data/supply_item_stock_comes_back_above_zero_03.csv")
       sup.reload
-      sup.stock.should == 12
+      expect(sup.stock).to eq 12
 
 
       prod.reload
-      prod.stock.should == 12
-      prod.is_available.should == true
+      expect(prod.stock).to eq 12
+      expect(prod.is_available).to be true
     end
 
     it "should check if a changed price makes a supply item cheaper than an existing one, then it should change the product to that" do
@@ -182,18 +181,18 @@ describe IngramUtil do
       sup = ingram.supply_items.where(:manufacturer_product_code => "123XXX45").first
       prod = Product.new_from_supply_item(sup)
       prod.save
-      prod.purchase_price.to_f.should == 5.0
+      expect(prod.purchase_price.to_f).to eq 5.0
 
       AlltronTestHelper.update_from_file(Rails.root + "spec/data/supply_item_price_change_makes_it_the_cheaper_option_02_alltron.csv")
       sup_alltron = alltron.supply_items.where(:manufacturer_product_code => "123XXX45").first
-      sup_alltron.purchase_price.to_f.should == 2.0
+      expect(sup_alltron.purchase_price.to_f).to eq 2.0
 
 
       # The product should have switched itself to this new option, the cheaper supply item from Alltron
       prod.reload
-      prod.supplier.should == alltron
-      prod.supply_item.should == sup_alltron
-      prod.purchase_price.to_f.should == 2.0
+      expect(prod.supplier).to eq alltron
+      expect(prod.supply_item).to eq sup_alltron
+      expect(prod.purchase_price.to_f).to eq 2.0
 
 
       # Product.update_price_and_stock
@@ -207,23 +206,19 @@ describe IngramUtil do
  
       CategoryDispatcher.destroy_all
       target_category = FactoryGirl.create(:category, {:name => 'Test'})
-      cd = FactoryGirl.create(:category_dispatcher, {:level_01 => 'Verbrauchsmaterial',
-                                                     :level_02 => 'Etiketten',
-                                                     :level_03 => 'Labeldrucker',
-                                                     :categories => [target_category]})
+      FactoryGirl.create(:category_dispatcher, {:level_01 => 'Verbrauchsmaterial',
+                                                :level_02 => 'Etiketten',
+                                                :level_03 => 'Labeldrucker',
+                                                :categories => [target_category]})
       IngramTestHelper.import_from_file(Rails.root + "spec/data/4_im.csv")
       # Only 3 out of 4 of the products in the file have a matching combination of categories to be automatically assigned
-      target_category.products.count.should == 3
-      SupplyItem.count.should == 4
-      # This doesn't pass on Travis
-      #Product.all.each do |p|
-      #  p.categories.include?(target_category).should == true
-      #end
+      expect(target_category.products.count).to eq 3
+      expect(SupplyItem.count).to eq 4
     end
 
     it "should import EAN codes for supply items" do
       IngramTestHelper.import_from_file(Rails.root + "spec/data/4_im.csv")
-      SupplyItem.count.should == 4
+      expect(SupplyItem.count).to eq 4
       SupplyItem.all.each do |si|
         expect(si.ean_code).to_not be("")
         expect(si.ean_code).to_not be_nil
@@ -245,7 +240,7 @@ describe IngramUtil do
     data[:description01] = "Some Description"
     data[:description02] = "Some Second Description"
     iu = IngramUtil.new
-    iu.construct_supply_item_name(data).should == "#{data[:name01]} #{data[:name02]} (#{data[:name03]})"
+    expect(iu.construct_supply_item_name(data)).to eq "#{data[:name01]} #{data[:name02]} (#{data[:name03]})"
   end
 
   it "should construct a default description for supply items" do
@@ -260,7 +255,7 @@ describe IngramUtil do
     data[:description01] = "Some Description"
     data[:description02] = "Some Second Description"
     iu = IngramUtil.new
-    iu.construct_supply_item_description(data).should == "#{data[:description01]} #{data[:description02]}"
+    expect(iu.construct_supply_item_description(data)).to eq "#{data[:description01]} #{data[:description02]}"
   end
 
 end
